ee4ea69a4fd39be793ec8ec0f012d8bd0c365717
ghazouani <ghazouani.ilhem@gmail.com>
2019-01-31 21:43:10 +0100

    setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="text-center">
+   <p> &copy; 2019 &middot; Laracarte by <a href="https://www.linkedin.com/in/ghazouani-ilhem-21a016132/">@etsmo</a>
+     &dash; A<a href="https://laramap.com"> laramp</a> app.
+   </p>
+   <p><b>this app has been built for learning purposes.</b></p>
+</footer>
---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.default')
+
+@section('content')
+<div class="container">
+    <h2 class="text-center"> What is Laracarte ?</h2>
+    <p>Laracarte is a clone app on <a href="https://laramap.com" target="_blank"> Laramap.com</a></p>
+
+    <div class="row">
+        <div class="col-md-6">
+            <p class="alert alert-warning">
+            <strong> <i class="fas fa-exclamation"></i>this app has been built by <a href="https://www.linkedin.com/in/ghazouani-ilhem-21a016132/">@etsma </a> learniing purcoses</strong>
+            </p>
+        </div>
+      
+</div>
+<p>feel fan to help to improve the <a href="#">source code</a></p>
+<hr>
+<h2> whats laramap ?</h2>
+<p> Laramap is the website by wich Laracarte was inspired :)</p>
+<p>More info <a href="https://laramap.com/p/about">here</a></p>
+<hr>
+<p>Basicaly it's built on Laravel &amp; bootstarp. But there's a bunch of services used for email and  other sections.</p>
+<ul>
+    <li>Laravel</li>
+    <li>bootstarp</li>
+    <li>Amazon</li>
+    <li>Google maps</li>
+    
+    
+    
+</ul>
+@stop 
